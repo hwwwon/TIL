14f82ae4507e1e539c3ecd47a6a4b0d1bd8f83a5
hwwwon <102505707+hwwwon@users.noreply.github.com>
2022-11-23 12:23:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        String reverse = new StringBuffer(my_string).reverse().toString();
+        
+        return reverse;
+    }
+}

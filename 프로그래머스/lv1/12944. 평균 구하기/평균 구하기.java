92009c3663abf5dcfa9235591b49a62ed78505a5
hwwwon <102505707+hwwwon@users.noreply.github.com>
2022-12-21 23:12:01 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+        for(int i : arr){
+            sum += i;
+        }
+        double answer = (double)sum / arr.length;
+        return answer;
+    }
+}

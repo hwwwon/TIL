823373796ea27c5c9dabb670856aad91e00915c0
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-01-15 17:00:53 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.62 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,15 +1,24 @@
 class Solution {
     public String solution(String s) {
+        // String answer = "";
+        // int cnt = 0;
+        // char[] arr = s.toCharArray();
+        // for(int i = 0; i < arr.length; i++){
+        //     String a = Character.toString(arr[i]);
+        //     cnt++;
+        //     if(a.equals(" ")) cnt = 0;
+        //     if(cnt % 2 == 1) a = a.toUpperCase();
+        //     else a = a.toLowerCase();
+        //     answer += a;
+        // }
+        // return answer;
         String answer = "";
         int cnt = 0;
-        char[] arr = s.toCharArray();
-        for(int i = 0; i < arr.length; i++){
-            String a = Character.toString(arr[i]);
-            cnt++;
-            if(a.equals(" ")) cnt = 0;
-            if(cnt % 2 == 1) a = a.toUpperCase();
-            else a = a.toLowerCase();
-            answer += a;
+        String[] array = s.split("");
+
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
         }
         return answer;
     }

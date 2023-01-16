d44e7b777219a77f102575b35b041f8d73d2b840
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-01-17 02:31:58 +0900

    [level 1] Title: 예산, Time: 0.37 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int cnt = 0;
+        Arrays.sort(d);
+        for(int i : d){
+            budget -= i;
+            if(budget < 0) break;
+            cnt++;
+        }
+        return cnt;
+    }
+}

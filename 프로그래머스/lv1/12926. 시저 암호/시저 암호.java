3d956c0515a20796c2169adb7d494f10ff0a2e73
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-01-17 12:37:16 +0900

    [level 1] Title: 시저 암호, Time: 33.26 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,11 @@
 class Solution {
     public String solution(String s, int n) {
-        char[] arr = s.toCharArray();
         String result = "";
-        for(char c : arr){
-            if(c >= 65 && c <= 90){
-                for(int i = 1; i <= n; i++){
-                    c += 1;
-                    if(c > 90) c = 65;
-                }
-            }else if(c >= 97 && c <= 122){
-                for(int i = 1; i <= n; i++){
-                    c += 1;
-                    if(c > 122) c = 97;
-                }
-            }
-            result += Character.toString(c);
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isUpperCase(c)) c = (char)((c - 'A' + n) % 26 + 'A');
+            else if(Character.isLowerCase(c)) c = (char)((c - 'a' + n) % 26 + 'a');
+            result += c;
         }
         return result;
     }

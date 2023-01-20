785546662b656c32adba878c768fdc13344003bb
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-01-20 16:48:50 +0900

    [level 1] Title: 최소직사각형, Time: 1.56 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,16 +1,22 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int row = 0, column = 0, tmp = 0;
-        for(int i = 0; i < sizes.length; i++){
-            if(sizes[i][0] < sizes[i][1]){
-                tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-            if(row < sizes[i][0]) row = sizes[i][0];
-            if(column < sizes[i][1]) column = sizes[i][1];
-        }
+//         int row = 0, column = 0, tmp = 0;
+//         for(int i = 0; i < sizes.length; i++){
+//             if(sizes[i][0] < sizes[i][1]){
+//                 tmp = sizes[i][0];
+//                 sizes[i][0] = sizes[i][1];
+//                 sizes[i][1] = tmp;
+//             }
+//             if(row < sizes[i][0]) row = sizes[i][0];
+//             if(column < sizes[i][1]) column = sizes[i][1];
+//         }
         
+//         return row*column;
+        int row = 0, column = 0;
+        for(int[] size : sizes){
+            row = Math.max(row, Math.max(size[0],size[1]));
+            column = Math.max(column, Math.min(size[0],size[1]));
+        }
         return row*column;
     }
 }

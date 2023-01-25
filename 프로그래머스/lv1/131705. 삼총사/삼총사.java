3e3f13f824428ffee13fffe38041dadef3103d6e
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-01-25 18:51:59 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] number) {
+        int chk = 0;
+        for(int i = 0; i < number.length; i++){
+            for(int j = i+1; j < number.length; j++){
+                for(int k = j+1; k < number.length; k++){
+                    if(number[i] + number[j] + number[k] == 0) chk++;
+                }
+            }
+        }
+        return chk;
+    }
+}

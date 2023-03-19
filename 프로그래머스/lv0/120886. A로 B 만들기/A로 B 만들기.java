0c5d0ce459ff277ad6c9901df13ee371291fccb3
hwwwon <102505707+hwwwon@users.noreply.github.com>
2023-03-19 13:47:43 +0900

    [level 0] Title: A로 B 만들기, Time: 0.30 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,15 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(String before, String after) {
-        int length = before.length();
         char[] b = before.toCharArray();
         char[] a = after.toCharArray();
         Arrays.sort(b);
         Arrays.sort(a);
-        for(int i = 0; i < before.length(); i++){
-            if(b[i] == a[i]) length -= 1;
-        }
-        if(length == 0) return 1;
-        return 0;
+        return new String(b).equals(new String(a)) ? 1 : 0;
     }
 }
